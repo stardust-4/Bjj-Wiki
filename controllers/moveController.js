20dac7e4d010182524c59a412d7b82ebd2435dc9
John <jboyle15@yahoo.com>
2022-10-26 00:36:38 -0500

    fixed findById switched to findOne({}),find by id works

--- controllers/moveController.js
+++ controllers/moveController.js
@@ -12,12 +12,35 @@ const getMoves = async (req, res) => {
     return res.status(500).send(error.message)
   }
 }
-const getMoveById = (req, res) => {
-  res.send({ move: `move with an id of ${req.params.id}` })
-}
-// const createMove = async (req, res) => {
-//   res.send(req.body)
+// const getMoveById = (req, res) => {
+//   res.send({ move: `move with an id of ${req.params.id}` })
 // }
+
+// const getPlantById = async (req, res) => {
+//   try {
+//     const { id } = req.params
+//     const plant = await Plant.findById(id)
+//     if (plant) {
+//       return res.status(200).json({ plant })
+//     }
+//     return res.status(404).send('Plant with the specified ID does not exists')
+//   } catch (error) {
+//     return res.status(500).send(error.message)
+//   }
+// }
+
+const getMoveById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const bjjMove = await BjjMove.findOne({ id })
+    if (bjjMove) {
+      return res.status(200).json({ bjjMove })
+    }
+    return res.status(404).send('BjjMove with the specified ID does not exists')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
 
 const createMove = async (req, res) => {
   try {
