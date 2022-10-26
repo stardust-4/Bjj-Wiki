38939e22849ce75d1d92c2210776a2f6903be56e
John <jboyle15@yahoo.com>
2022-10-26 01:36:19 -0500

    delete BY ID works in insomnia

--- controllers/moveController.js
+++ controllers/moveController.js
@@ -1,9 +1,4 @@
-// import myMovesDb from ('./db/moves')
-// const getMoves = (req, res) => {
-//   res.send({ myMovesDb })
-// }
 const BjjMove = require('../models/bjjMove')
-
 const getMoves = async (req, res) => {
   try {
     const moves = await BjjMove.find()
@@ -12,23 +7,6 @@ const getMoves = async (req, res) => {
     return res.status(500).send(error.message)
   }
 }
-// const getMoveById = (req, res) => {
-//   res.send({ move: `move with an id of ${req.params.id}` })
-// }
-
-// const getPlantById = async (req, res) => {
-//   try {
-//     const { id } = req.params
-//     const plant = await Plant.findById(id)
-//     if (plant) {
-//       return res.status(200).json({ plant })
-//     }
-//     return res.status(404).send('Plant with the specified ID does not exists')
-//   } catch (error) {
-//     return res.status(500).send(error.message)
-//   }
-// }
-
 const getMoveById = async (req, res) => {
   try {
     const { id } = req.params
@@ -41,7 +19,6 @@ const getMoveById = async (req, res) => {
     return res.status(500).send(error.message)
   }
 }
-
 const createMove = async (req, res) => {
   try {
     const bjjMove = await new BjjMove(req.body)
@@ -53,9 +30,22 @@ const createMove = async (req, res) => {
     return res.status(500).json({ error: error.message })
   }
 }
+const deleteMove = async (req, res) => {
+  try {
+    const { id } = req.params
+    const deleted = await BjjMove.findOneAndDelete({ id })
+    if (deleted) {
+      return res.status(200).send('BjjMove deleted')
+    }
+    throw new Error('BjjMove not found')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
 
 module.exports = {
   getMoves,
   getMoveById,
-  createMove
+  createMove,
+  deleteMove
 }
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const moveController = require('../controllers/moveController')
 router.get('/moves', moveController.getMoves)
 router.get('/move/:id', moveController.getMoveById)
 router.post('/create', moveController.createMove)
+router.delete('/delete/move/:id', moveController.deleteMove)
 
 module.exports = router
